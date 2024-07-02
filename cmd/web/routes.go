ba671df8f0f81ce2af105ad2bf52f747e5e06f60
gllkmike1445 <88540172+gllkmike1445@users.noreply.github.com>
2024-07-02 18:21:11 +0300

    refactored packages and added some new JS functions that works between get post

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/gllkmike1445/bookings/pkg/config"
-	"github.com/gllkmike1445/bookings/pkg/handlers"
+	"github.com/gllkmike1445/bookings/internal/config"
+	"github.com/gllkmike1445/bookings/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
@@ -24,7 +24,11 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
+
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
+
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
